9ef1a2ca2caeec123f490e43fddcff59103f6e43
ahmedhamza47 <muhammadhamza12342@gmail.com7>
2022-10-21 11:44:11 +0545

    completed upto ans no 7

--- JS fundamental Part1/script.js
+++ JS fundamental Part1/script.js
@@ -36,3 +36,79 @@ console.log(`Length of object is ${ObjLength}`);
 // alert(`The volume of cylinder is ${Cylinder(height, radius)}`);
 
 //Ans No 3
+//Program to create a Clock Note. where output will come every second
+
+const clock = function () {
+  let currDate = new Date();
+  let hour = currDate.getHours();
+  let minute = currDate.getMinutes();
+  let second = currDate.getSeconds();
+
+  hour = hour < 10 ? "0" + hour : hour;
+
+  minute = minute < 10 ? "0" + minute : minute;
+
+  second = second < 10 ? "0" + second : second;
+
+  let time = hour + ":" + minute + ":" + second + " ";
+  document.getElementById("clock").innerHTML = time;
+  let t = setTimeout(function () {
+    clock();
+  }, 1000);
+};
+clock();
+
+//ans no 4
+
+function FindAllMethods(obj) {
+  return Object.getOwnPropertyNames(obj).filter(function (property) {
+    return typeof obj[property] == "function";
+  });
+}
+const Student = {
+  name: "Hamza Ahmed",
+  age: 47,
+  getName() {
+    return this.name;
+  },
+  calcAge() {
+    this.age > 18 ? `Adult` : `child`;
+  },
+};
+
+const methods = FindAllMethods(Student);
+console.log(`The methods inside object Student is  ${methods}`);
+
+//ans no 5
+//Js function to convert an object intto list of [key,value ] pairs
+
+function convertToKeyValuePair(obj) {
+  return Object.entries(obj);
+}
+const sampleObject = {
+  fullName: "Hamza AHmed",
+  rollNO: 47,
+};
+console.log(convertToKeyValuePair(sampleObject));
+
+//ans no 6
+//JS program to reverse a number
+
+const reversedNum = (num) =>
+  parseFloat(num.toString().split("").reverse().join("")) * Math.sign(num);
+
+console.log(reversedNum(-1.567));
+console.log(reversedNum(260.05));
+
+//ans no 7
+
+const combination = (str) => {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length + 1; j++) {
+      arr.push(str.slice(i, j));
+    }
+  }
+  console.log(arr);
+};
+combination("hamza");
